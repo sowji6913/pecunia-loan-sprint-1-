662e67c58aab770007aa20b12ffbcd192654fde1
dell <dell@DESKTOP-82534KQ>
2020-02-24 18:13:40 +0530

    project

--- PecuniaLoan/src/main/java/com/cg/pecunia/ui/Client.java
+++ PecuniaLoan/src/main/java/com/cg/pecunia/ui/Client.java
@@ -10,7 +10,7 @@ import com.cg.pecunia.service.LoanServiceImpl;
 public class Client {
 public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		Scanner scanner = new Scanner(System.in);
+		 Scanner scanner = new Scanner(System.in);
 		Loan loan = null;
 		LoanService loanService = new LoanServiceImpl();
 		List<Loan> list= null;
